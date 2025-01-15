053b53b7b2f5165488b0373db3a83659a3f447d0
yewon_s <seoyewon93@gmail.com>
2025-01-15 13:00:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const shareBtn = document.querySelector('.share-btn')
+const shareOptions = document.querySelector('.share-options')
+
+
+shareBtn.addEventListener("click", function() {
+    shareOptions.classList.toggle('active-state')
+})
